d4fcb4e1fee5457fa63e74a6b2111e255529dcae
acme_pjz <acme_pjz@hotmail.com>
2015-09-06 23:52:16 +0800

    test LOD

--- jni/src/main.cpp
+++ jni/src/main.cpp
@@ -7,6 +7,7 @@
 #include <osg/MatrixTransform>
 #include <osg/PolygonMode>
 #include <osg/PolygonOffset>
+#include <osg/LOD>
 #include <osgUtil/Optimizer>
 #include <osgViewer/ViewerEventHandlers>
 #include <osgFX/SpecularHighlights>
@@ -42,6 +43,30 @@ osg::Node* test(){
 		geode->addDrawable(ground);
 		geode->addDrawable(wireframe);
 	}
+	osg::ref_ptr<osg::LOD> lod = new osg::LOD;
+	lod->addChild(geode, 0.0f, 100.0f);
+	geode = new osg::Geode;
+	{
+		osg::ref_ptr<osg::Geometry> ground = createCube(
+			osg::Vec3(0, 0, -0.25f),
+			osg::Vec3(1, 1, 0),
+			false,
+			0.0f,
+			osg::Vec3(0.4f, 0.4f, 0.4f)
+			);
+		osg::ref_ptr<osg::Geometry> wireframe = createCube(
+			osg::Vec3(0, 0, -0.25f),
+			osg::Vec3(1, 1, 0),
+			true,
+			0.0f,
+			osg::Vec3(0.2f, 0.2f, 0.2f)
+			);
+		wireframe->getOrCreateStateSet()->setMode(GL_LIGHTING, osg::StateAttribute::OFF);
+		ground->getOrCreateStateSet()->setAttributeAndModes(new osg::PolygonOffset(1.0f, 1.0f));
+		geode->addDrawable(ground);
+		geode->addDrawable(wireframe);
+	}
+	lod->addChild(geode, 100.0f, FLT_MAX);
 
 	//create some tile types
 	osg::ref_ptr<TileType> ground = new TileType;
@@ -49,7 +74,7 @@ osg::Node* test(){
 	ground->index = 1;
 	ground->name = "Ground";
 	ground->desc = "Normal ground.";
-	ground->appearance = geode;
+	ground->appearance = lod;
 
 	osg::ref_ptr<game::MapData> map = new game::MapData;
 	map->resize(osg::Vec3i(), osg::Vec3i(10, 10, 1), false);
