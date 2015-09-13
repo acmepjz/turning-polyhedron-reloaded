89f3d51bb0bab11bd494f52a3948ffcb6d6c72c7
acme_pjz <acme_pjz@hotmail.com>
2015-09-13 18:44:17 +0800

    Merged Cuboid.h to Polyhedron.h

--- jni/src/Polyhedron.cpp
+++ jni/src/Polyhedron.cpp
@@ -6,15 +6,22 @@ namespace game {
 
 	Polyhedron::Polyhedron()
 		: flags(0)
+		, size(1, 1, 2)
+		, customShapeEnabled(false)
+		, customShape(1, 1)
 	{
 	}
 
 	Polyhedron::Polyhedron(const Polyhedron& other, const osg::CopyOp& copyop)
 		: Object(other, copyop)
+		, id(other.id)
 		, objType(other.objType)
 		, flags(other.flags)
 		, pos(other.pos)
-		, appearance(copyop(other.appearance))
+		, lbound(other.lbound)
+		, size(other.size)
+		, customShapeEnabled(other.customShapeEnabled)
+		, customShape(other.customShape)
 	{
 
 	}
@@ -23,13 +30,64 @@ namespace game {
 	{
 	}
 
+	unsigned char& Polyhedron::operator()(int x, int y, int z){
+		int idx = customShapeEnabled ? ((z - lbound.z())*size.y() + y - lbound.y())*size.x() + x - lbound.x() : 0;
+		return customShape[idx];
+	}
+
+	unsigned char Polyhedron::operator()(int x, int y, int z) const{
+		int idx = customShapeEnabled ? ((z - lbound.z())*size.y() + y - lbound.y())*size.x() + x - lbound.x() : 0;
+		return customShape[idx];
+	}
+
+	void Polyhedron::resize(const osg::Vec3i& lbound_, const osg::Vec3i& size_, bool customShape_, bool preserved){
+		bool old = customShapeEnabled;
+		customShapeEnabled = customShape_;
+
+		if (!(preserved && customShape_ && old)) {
+			size = size_;
+
+			unsigned char c = preserved ? customShape[0] : 1;
+			if (c == 0) c = 1;
+			customShape.resize(customShape_ ? size_.x()*size_.y()*size_.z() : 1, c);
+
+			return;
+		}
+
+		std::vector<unsigned char> tmp = customShape;
+		customShape.resize(size_.x()*size_.y()*size_.z(), 0);
+
+#define SX(X) s##X = lbound.X() > lbound_.X() ? lbound.X() : lbound_.X()
+#define EX(X) e##X = (lbound.X() + size.X() < lbound_.X() + size_.X()) ? \
+	(lbound.X() + size.X()) : (lbound_.X() + size_.X())
+		const int SX(x), EX(x), SX(y), EX(y), SX(z), EX(z);
+#undef SX
+#undef EX
+
+		for (int z = sz; z < ez; z++) {
+			for (int y = sy; y < ey; y++) {
+				for (int x = sx; x < ex; x++) {
+					int old_idx = ((z - lbound.z())*size.y() + y - lbound.y())*size.x() + x - lbound.x();
+					int new_idx = ((z - lbound_.z())*size_.y() + y - lbound_.y())*size_.x() + x - lbound_.x();
+					customShape[new_idx] = tmp[old_idx];
+				}
+			}
+		}
+
+		lbound = lbound_;
+		size = size_;
+	}
+
 	REG_OBJ_WRAPPER(game, Polyhedron, "")
 	{
 		ADD_STRING_SERIALIZER(id, "");
 		ADD_STRING_SERIALIZER(objType, "");
 		ADD_INT_SERIALIZER(flags, 0);
 		ADD_REF_ANY_SERIALIZER(pos, MapPosition, MapPosition());
-		ADD_OBJECT_SERIALIZER(appearance, osg::Node, NULL);
+		ADD_VEC3I_SERIALIZER(lbound, osg::Vec3i());
+		ADD_VEC3I_SERIALIZER(size, osg::Vec3i(1, 1, 2));
+		ADD_BOOL_SERIALIZER(customShapeEnabled, false);
+		ADD_VECTOR_SERIALIZER(customShape, std::vector<unsigned char>, osgDB::BaseSerializer::RW_UCHAR, 32);
 	}
 
 }
--- jni/src/Polyhedron.h
+++ jni/src/Polyhedron.h
@@ -11,6 +11,20 @@ namespace game {
 
 	class ObjectType;
 
+	/** Represents a polyhedron.
+	* A polyhedron has the following properties:
+	* * Shape:
+	*   * cuboid (with size and custom shape)
+	*   * a list of predefined regular polyhedra
+	* * Movement:
+	*   * rolling (e.g. boxes in Rolling Block Mazes/Bloxorz)
+	*   * moving (e.g. boxes in Sokoban/PuzzleBoy)
+	*   * rotating (e.g. rotating blocks in PuzzleBoy)
+	* * Controller:
+	*   * none (i.e. can only be pushed by player)
+	*   * player (i.e. can only be controlled by player)
+	*/
+
 	class Polyhedron :
 		public osg::Object
 	{
@@ -22,6 +36,17 @@ namespace game {
 		Polyhedron();
 		Polyhedron(const Polyhedron& other, const osg::CopyOp& copyop = osg::CopyOp::SHALLOW_COPY);
 
+		unsigned char& operator()(int x, int y, int z);
+		unsigned char& operator()(const osg::Vec3i& p) {
+			return operator()(p.x(), p.y(), p.z());
+		}
+		unsigned char operator()(int x, int y, int z) const;
+		unsigned char operator()(const osg::Vec3i& p) const {
+			return operator()(p.x(), p.y(), p.z());
+		}
+
+		void resize(const osg::Vec3i& lbound_, const osg::Vec3i& size_, bool customShape_, bool preserved);
+
 	public:
 		std::string id; //!< the polyhedron id
 
@@ -29,13 +54,24 @@ namespace game {
 		int flags; //!< the polyhedron flags
 		MapPosition pos; //!< the start position
 
-		osg::ref_ptr<osg::Node> appearance; //!< the appearance
+		osg::Vec3i lbound; //!< lower bound of polyhedron data, which in turn determines the center of polyhedron
+		osg::Vec3i size; //!< the size of polyhedron
+
+		bool customShapeEnabled; //!< use custom shape. if it is true then \c customShape is used, otherwise the polyhedron is a solid cuboid.
+		std::vector<unsigned char> customShape; //!< the custom shape
 
 		UTIL_ADD_BYREF_GETTER_SETTER(std::string, id);
 		UTIL_ADD_BYREF_GETTER_SETTER(std::string, objType);
 		UTIL_ADD_BYVAL_GETTER_SETTER(int, flags);
 		UTIL_ADD_BYREF_GETTER_SETTER(MapPosition, pos);
-		UTIL_ADD_OBJ_GETTER_SETTER(osg::Node, appearance);
+		UTIL_ADD_BYREF_GETTER_SETTER(osg::Vec3i, lbound);
+		UTIL_ADD_BYREF_GETTER_SETTER(osg::Vec3i, size);
+		UTIL_ADD_BYVAL_GETTER_SETTER(bool, customShapeEnabled);
+		UTIL_ADD_BYREF_GETTER_SETTER(std::vector<unsigned char>, customShape);
+
+	public:
+		//the following properties don't save to file and is generated at runtime
+		osg::ref_ptr<osg::Node> _appearance; //!< the appearance
 	};
 
 }
